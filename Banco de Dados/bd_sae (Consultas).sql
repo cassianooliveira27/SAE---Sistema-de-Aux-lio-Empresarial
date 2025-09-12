b57f880a90ae1ac37136f31639d7d3860e70437b
Jaozim221 <jhf.am.2008@gmail.com>
2025-09-12 06:57:13 -0300

    Update bd_sae (Consultas).sql

--- Banco de Dados/bd_sae (Consultas).sql
+++ Banco de Dados/bd_sae (Consultas).sql
@@ -1,35 +1,52 @@
 USE sae_db;
 
-SELECT * FROM cargos;
+---------------------------------------------------
+-- CONSULTA DE CARGOS
+---------------------------------------------------
 
-SELECT * FROM cargos
-WHERE nome_cargo LIKE '%Analista%';
+-- 🔹 Lista todos os cargos, ordenados por nome
+SELECT *
+FROM cargos
+ORDER BY nome_cargo;
 
-SELECT * FROM cargos
-WHERE id_cargo = 1;
+-- 🔹 Pesquisa cargos pelo nome (exemplo: buscar "Analista")
+SELECT *
+FROM cargos
+WHERE nome_cargo LIKE '%Analista%'
+ORDER BY nome_cargo;
 
+
+---------------------------------------------------
+-- CONSULTA DE FUNCIONÁRIOS
+---------------------------------------------------
+
+-- 🔹 Lista todos os funcionários com seus cargos, ordenados por nome
 SELECT f.id_funcionario, f.nome_funcionario, f.cpf_funcionario,
-       f.salario, f.data_admissao, c.nome_cargo
+       f.salario, f.data_admissao, c.nome_cargo, c.carga_horaria_max
 FROM funcionarios f
-JOIN cargos c ON f.id_cargo = c.id_cargo;
+LEFT JOIN cargos c ON f.id_cargo = c.id_cargo
+ORDER BY f.nome_funcionario;
 
+-- 🔹 Pesquisa funcionários pelo nome
 SELECT f.id_funcionario, f.nome_funcionario, f.cpf_funcionario,
-       f.salario, f.data_admissao, c.nome_cargo
+       f.salario, f.data_admissao, c.nome_cargo, c.carga_horaria_max
 FROM funcionarios f
-JOIN cargos c ON f.id_cargo = c.id_cargo
-WHERE f.nome_funcionario LIKE '%Carlos%';
+LEFT JOIN cargos c ON f.id_cargo = c.id_cargo
+WHERE f.nome_funcionario LIKE '%Carlos%'
+ORDER BY f.nome_funcionario;
 
+-- 🔹 Filtra funcionários por cargo específico (exemplo: id_cargo = 2)
 SELECT f.id_funcionario, f.nome_funcionario, f.cpf_funcionario,
-       f.salario, f.data_admissao, c.nome_cargo
+       f.salario, f.data_admissao, c.nome_cargo, c.carga_horaria_max
 FROM funcionarios f
-JOIN cargos c ON f.id_cargo = c.id_cargo
-WHERE f.cpf_funcionario = '123.456.789-00';
+LEFT JOIN cargos c ON f.id_cargo = c.id_cargo
+WHERE f.id_cargo = 2
+ORDER BY f.nome_funcionario;
 
+-- 🔹 Filtra funcionários por faixa salarial (mínimo e máximo)
 SELECT f.id_funcionario, f.nome_funcionario, f.cpf_funcionario,
-       f.salario, f.data_admissao, c.nome_cargo
+       f.salario, f.data_admissao, c.nome_cargo, c.carga_horaria_max
 FROM funcionarios f
-JOIN cargos c ON f.id_cargo = c.id_cargo
-WHERE c.nome_cargo LIKE '%Assistente%';
-
-SELECT * FROM funcionarios
-WHERE id_funcionario = 2;
+LEFT JOIN cargos c ON f.id_cargo = c.id_cargo
+WHERE f.salario >= 2000 AND f.salario <= 8000
+ORDER BY f.nome_funcionario;
